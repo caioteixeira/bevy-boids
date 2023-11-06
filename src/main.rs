88d27b7532620fea2e9751d84df3b53b0665476d
Caio Teixeira <caiovmteixeira@outlook.com>
2023-11-06 07:02:33 -0300

    Simple seek behavior

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,26 @@
 
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Velocity(Vec3);
+
+#[derive(Component)]
+struct Acceleration(Vec3);
+
+#[derive(Component)]
+struct Target(Vec3);
+
+#[derive(Component)]
+struct MaxSpeed(f32);
+
+#[derive(Component)]
+struct MaxForce(f32);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, (seek, apply_acceleration, update_position))
         .run();
 }
 
@@ -17,13 +33,46 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
     let ship_texture_handle = asset_server.load("ship_C.png");
-    commands.spawn(SpriteBundle {
-        texture: ship_texture_handle.clone(),
-        transform: Transform {
-            translation: Vec3::new(0., 0., 0.),
-            scale: Vec3::new(0.3, 0.3, 0.),
-            ..Default::default()
+    commands.spawn((
+        SpriteBundle {
+            texture: ship_texture_handle.clone(),
+            transform: Transform {
+                translation: Vec3::new(80., 50., 0.),
+                scale: Vec3::new(0.3, 0.3, 0.),
+                ..Default::default()
+            },
+            ..default()
         },
-        ..default()
-    });
+        Velocity(Vec3::new(0., 0., 0.)),
+        Acceleration(Vec3::new(0., 0., 0.)),
+        Target(Vec3::new(100., 500., 0.)),
+        MaxSpeed(10.),
+        MaxForce(1.),
+    ));
+}
+
+fn seek(mut query: Query<(&Transform, &Target, &mut Acceleration, &MaxSpeed, &MaxForce)>) {
+    for (transform, target, mut acceleration, max_speed, max_force) in query.iter_mut() {
+        let mut desired_velocity = target.0 - transform.translation;
+        desired_velocity = desired_velocity.normalize() * max_speed.0;
+
+        let mut steering = desired_velocity - transform.translation;
+        steering = steering.normalize() * max_force.0;
+
+        acceleration.0 += steering;
+    }
+}
+
+fn apply_acceleration(mut query: Query<(&mut Velocity, &Acceleration, &MaxSpeed)>) {
+    for (mut velocity, acceleration, max_speed) in query.iter_mut() {
+        velocity.0 += acceleration.0;
+        velocity.0 = velocity.0.normalize() * max_speed.0;
+    }
+}
+
+fn update_position(mut query: Query<(&mut Transform, &Velocity)>) {
+    for (mut transform, velocity) in query.iter_mut() {
+        transform.translation += velocity.0;
+        transform.rotation = Quat::from_rotation_z(velocity.0.y.atan2(velocity.0.x) + 180.);
+    }
 }
